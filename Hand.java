55bda43101c62f1d503dac79dd917f33dcbc9fe8
Logan Clevenger <logan.clevenger@outlook.com>
2026-09-14 20:26:37 -0700

    Added rollDice method

--- Hand.java
+++ Hand.java
@@ -1,11 +1,19 @@
 public class Hand {
     Die[] dice;
+    boolean[] inMeld;
 
     public Hand() {
         dice = new Die[6];
+        inMeld = new boolean[6];
 
         for (int i = 0; i < 6; i++) {
             dice[i] = new Die(1);
+            inMeld[i] = false;
+        }
+    }
+    public void rollDice() {
+        for (int i = 0; i < 6; i++) {
+            dice[i].roll();
         }
     }
 }
