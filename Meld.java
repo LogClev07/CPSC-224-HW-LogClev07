30bfaf37e0bcb35bb712c0c88e9e3108b616dcc5
Logan Clevenger <logan.clevenger@outlook.com>
2026-09-15 22:16:50 -0700

    Added calculateScore() to Meld.java

--- Meld.java
+++ Meld.java
@@ -1,23 +1,125 @@
 import java.util.ArrayList;
+
 public class Meld {
 
     private ArrayList<Die> dice;
 
     public Meld() {
         dice = new ArrayList<>();
-
     }
+
     public void addDie(Die die) {
         dice.add(die);
     }
+
     public void removeDie(Die die) {
         dice.remove(die);
     }
+
     public int[] findMeldValueTotals() {
         int[] meldValueTotals = new int[7];
+
         for (Die die : dice) {
             meldValueTotals[die.getValue()]++;
         }
+
         return meldValueTotals;
+    }
+    // Used ChatGPT to give hints regarding how to tackle scoring priority
+    public int calculateScore() {
+        int score = 0;
+        int numSoloValues = 0;
+        int numDoubles = 0;
+
+        boolean tripleOnes = false;
+        boolean tripleTwos = false;
+        boolean tripleThrees = false;
+        boolean tripleFours = false;
+        boolean tripleFives = false;
+        boolean tripleSixes = false;
+
+        int[] meldValueTotals = findMeldValueTotals();
+
+        // Finding the number of singles, doubles, and triple values
+        for (int i = 1; i <= 6; i++) {
+
+            if (meldValueTotals[i] == 1) {
+                numSoloValues++;
+            } else if (meldValueTotals[i] == 2) {
+                numDoubles++;
+            } else if (meldValueTotals[i] >= 3) {
+
+                if (i == 1) {
+                    tripleOnes = true;
+                } else if (i == 2) {
+                    tripleTwos = true;
+                } else if (i == 3) {
+                    tripleThrees = true;
+                } else if (i == 4) {
+                    tripleFours = true;
+                } else if (i == 5) {
+                    tripleFives = true;
+                } else if (i == 6) {
+                    tripleSixes = true;
+                }
+            }
+        }
+        // Checking for possible straight
+        if (numSoloValues == 6) {
+            return 1000;
+        }
+
+        // Checking for triple doubles
+        else if (numDoubles == 3) {
+            return 750;
+        }
+
+        // Scoring triple ones
+        if (tripleOnes) {
+            int extraValues = meldValueTotals[1] - 3;
+            score = score + 1000 + (100 * extraValues);
+        }
+
+        // Scoring triple twos
+        if (tripleTwos) {
+            int extraValues = meldValueTotals[2] - 3;
+            score = score + 200 + (200 * extraValues);
+        }
+
+        // Scoring triple threes
+        if (tripleThrees) {
+            int extraValues = meldValueTotals[3] - 3;
+            score = score + 300 + (300 * extraValues);
+        }
+
+        // Scoring triple fours
+        if (tripleFours) {
+            int extraValues = meldValueTotals[4] - 3;
+            score = score + 400 + (400 * extraValues);
+        }
+
+        // Scoring triple fives
+        if (tripleFives) {
+            int extraValues = meldValueTotals[5] - 3;
+            score = score + 500 + (500 * extraValues);
+        }
+
+        // Scoring triple sixes
+        if (tripleSixes) {
+            int extraValues = meldValueTotals[6] - 3;
+            score = score + 600 + (600 * extraValues);
+        }
+
+        // Scoring leftover ones
+        if (tripleOnes == false) {
+            score = score + (meldValueTotals[1] * 100);
+        }
+
+        // Scoring leftover fives
+        if (tripleFives == false) {
+            score = score + (meldValueTotals[5] * 50);
+        }
+
+        return score;
     }
 }
